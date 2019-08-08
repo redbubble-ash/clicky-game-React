6d8394580c4c4de90bd545efa9cdf3f9bf548240
Your Name <xiejing418@gmail.com>
2019-08-08 03:01:00 -0700

    Updates

---
+++ precache-manifest.2af7d83092ace222093c0cac44296131.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c9b9f47ae8f82ba611cea7467f2f1c6c",
+    "url": "/clicky-game-react/index.html"
+  },
+  {
+    "revision": "58a4978e3e4d9a6e2db0",
+    "url": "/clicky-game-react/static/css/main.a972b15b.chunk.css"
+  },
+  {
+    "revision": "45124f853ba07aa965c0",
+    "url": "/clicky-game-react/static/js/2.6efc73d3.chunk.js"
+  },
+  {
+    "revision": "58a4978e3e4d9a6e2db0",
+    "url": "/clicky-game-react/static/js/main.03fdb326.chunk.js"
+  },
+  {
+    "revision": "96e9a9b550dae971e8fe",
+    "url": "/clicky-game-react/static/js/runtime~main.4ef103c0.js"
+  }
+]);
